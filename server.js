ff5f4e4f5a0b58a7ac73a72ddb492c7d1a0f86bd
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-02-08 03:03:51 -0800

    finding my feet

--- server.js
+++ server.js
@@ -7,3 +7,30 @@
 //        .get(controller.get_distance);
 // };
 
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const aboutRouter = require("./routes/about");
+const weatherRouter = require("./routes/weather");
+
+const PORT = 3000;
+const HOST_NAME = "localhost";
+
+const app = express();
+app.use(express.static("client"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/api-gateway", apiGatewayRouter);
+
+// app.use("/weather", weatherRouter);
+// app.use("/about", aboutRouter);
+
+
+// app.listen(PORT, HOST_NAME, ()=&gt;{
+//     console.log(`Server running at ${HOST_NAME}:${PORT}`)
+// })
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
